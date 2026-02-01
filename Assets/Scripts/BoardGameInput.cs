32f2a4988a753256e010b370d32910c93bb550d8
Hunter Dyar <bloop@hdyar.com>
2026-01-31 22:56:28 -0500

    CHARGE SHOTS

--- Assets/Scripts/BoardGameInput.cs
+++ Assets/Scripts/BoardGameInput.cs
@@ -27,15 +27,56 @@ public class BoardGameInput : MonoBehaviour
 		
 	}
 
+	private Dictionary<int, bool> _touchState = new Dictionary<int, bool>();
 	private void ProcessGlyphs()
 	{
 		var contacts = BoardInput.GetActiveContacts(BoardContactType.Glyph);
 		foreach (var contact in contacts)
 		{
 			var id = contact.glyphId;
+			var phase = UpdateTouchPhase(id, contact.isTouched);
 			if (_transforms.TryGetValue(id, out var piece))
 			{
-				piece.ProcessInput(contact);
+				piece.ProcessInput(contact, phase);
+			}
+		}
+	}
+
+	private TouchPhase UpdateTouchPhase(int id, bool touched)
+	{
+		bool prev = false;
+		if (_touchState.ContainsKey(id))
+		{
+			prev = _touchState[id];
+			_touchState[id] = touched;
+		}
+		else
+		{
+			_touchState.Add(id,touched);
+		}
+
+		if (prev)
+		{
+			//was touching
+			if (touched)
+			{
+				return TouchPhase.Stay;
+			}
+			else
+			{
+				return TouchPhase.Release;
+			}
+		}
+		else
+		{
+			//wasn't touching
+			if (touched)
+			{
+				return TouchPhase.Press;
+			}
+			else
+			{
+				return TouchPhase.None;
 			}
 		}
 	}
@@ -47,4 +88,12 @@ public class BoardGameInput : MonoBehaviour
 			Debug.LogWarning($"Already a piece mapped to glyphID {glyphID}. Can't add another.");
 		}
 	}
+}
+
+public enum TouchPhase
+{
+	Press,
+	Stay,
+	Release,
+	None
 }
